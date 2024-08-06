bb360d4b17df182393faed90ed56f612ba37134d
Gylvaris <111581734+Gylvaris@users.noreply.github.com>
2024-08-06 16:36:48 +0200

    System update and nvidia changes

--- modules/system/gpu/nvidia.nix
+++ modules/system/gpu/nvidia.nix
@@ -4,13 +4,16 @@
     modesetting.enable = true;
     dynamicBoost.enable = true;
     powerManagement.enable = true;
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     prime = {
-      offload = {
+      #offload = {
+      #  enable = true;
+      #  enableOffloadCmd = true;
+      #};
+      sync = {
         enable = true;
-        enableOffloadCmd = true;
       };
       amdgpuBusId = "PCI:5:0:0";
       nvidiaBusId = "PCI:1:0:0";
