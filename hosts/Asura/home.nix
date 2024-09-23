0cb0690b3e9a64a54d5a8f670ed9620b4a40917c
Gylvaris <111581734+Gylvaris@users.noreply.github.com>
2024-09-23 10:55:47 +0200

    add commit signature verification

--- hosts/Asura/home.nix
+++ hosts/Asura/home.nix
@@ -1,4 +1,5 @@
 {
+  config, 
   inputs,
   lib,
   pkgs,
@@ -57,7 +58,21 @@
     enable = true;
     userName = "Gylvaris";
     userEmail = "111581734+Gylvaris@users.noreply.github.com";
+
+    signing = {
+      key = "~/.ssh/id_ed25519";
+      signByDefault = true;
+    };
+
+    extraConfig = {
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+    };
   };
+
+  xdg.configFile."git/allowed_signers".text = ''
+    ${config.programs.git.userEmail} namespaces="git" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG15d9WSIX2Xcd9pRfSCg43UHoyEGJeCaHxwWGK/QcKA"
+  '';
 
   programs.starship = {
     enable = true;
