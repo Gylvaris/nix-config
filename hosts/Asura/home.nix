671fb8bd59967419887837c9141fdb0b4079bab5
Gylvaris <111581734+Gylvaris@users.noreply.github.com>
2024-10-06 11:08:00 +0200

    re-add github key

--- hosts/Asura/home.nix
+++ hosts/Asura/home.nix
@@ -71,7 +71,7 @@
   };
 
   xdg.configFile."git/allowed_signers".text = ''
-    ${config.programs.git.userEmail} namespaces="git" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG15d9WSIX2Xcd9pRfSCg43UHoyEGJeCaHxwWGK/QcKA"
+    ${config.programs.git.userEmail} namespaces="git" ""ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTrQ4GAyRrgoSEa19NPoHTw92mhLEbLMZMH+sTGbVt6
   '';
 
   programs.starship = {
